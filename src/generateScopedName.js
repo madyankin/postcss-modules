56819eeed08546773d9d09ecedeb886ec0b0a44c
Alexander Madyankin <alexander@madyankin.name>
2016-05-27 17:20:31 +0300

    Rename variable in generateScopedName to improve readability

--- src/generateScopedName.js
+++ src/generateScopedName.js
@@ -1,9 +1,9 @@
 import stringHash from 'string-hash';
 
 export default function generateScopedName(name, filename, css) {
-  const i         = css.indexOf(`.${ name }`);
-  const numLines  = css.substr(0, i).split(/[\r\n]/).length;
-  const hash      = stringHash(css).toString(36).substr(0, 5);
+  const i           = css.indexOf(`.${ name }`);
+  const lineNumber  = css.substr(0, i).split(/[\r\n]/).length;
+  const hash        = stringHash(css).toString(36).substr(0, 5);
 
-  return `_${ name }_${ hash }_${ numLines }`;
+  return `_${ name }_${ hash }_${ lineNumber }`;
 }
