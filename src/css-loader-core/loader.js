c2fe1a7ac2b6b42484a76dbbc61af30aa87c115d
Florian Proksch <florian.proksch@protonmail.com>
2021-01-15 20:58:26 +0100

    Refactor loader dependencies
    Isolates the file loader so it does no longer depend on the parser.
    This frees the loader to only care aout loading files, which makes it
    easier to inject custom loaders via the provided interface.
    
    Since the loader is now only a single function that is extracted
    immediately, it's been reduced to a function instead of a class.

--- src/css-loader-core/loader.js
+++ src/css-loader-core/loader.js
@@ -1,112 +1,26 @@
-// Copied from https://github.com/css-modules/css-modules-loader-core
-
-import postcss from "postcss";
 import fs from "fs";
 import path from "path";
 
-import Parser from "./parser";
+export default (newPath, relativeTo, root) => {
+  return new Promise((resolve, reject) => {
+    let relativeDir = path.dirname(relativeTo),
+      absolutePath = path.resolve(
+        path.join(root, relativeDir),
+        newPath
+      );
 
-class Core {
-  constructor(plugins) {
-    this.plugins = plugins || Core.defaultPlugins;
-  }
-
-  load(sourceString, sourcePath, trace, pathFetcher) {
-    let parser = new Parser(pathFetcher, trace);
-
-    return postcss(this.plugins.concat([parser.plugin()]))
-      .process(sourceString, { from: "/" + sourcePath })
-      .then((result) => {
-        return {
-          injectableSource: result.css,
-          exportTokens: parser.exportTokens,
-        };
-      });
-  }
-}
-
-// Sorts dependencies in the following way:
-// AAA comes before AA and A
-// AB comes after AA and before A
-// All Bs come after all As
-// This ensures that the files are always returned in the following order:
-// - In the order they were required, except
-// - After all their dependencies
-const traceKeySorter = (a, b) => {
-  if (a.length < b.length) {
-    return a < b.substring(0, a.length) ? -1 : 1;
-  } else if (a.length > b.length) {
-    return a.substring(0, b.length) <= b ? -1 : 1;
-  } else {
-    return a < b ? -1 : 1;
-  }
-};
-
-export default class FileSystemLoader {
-  constructor(root, plugins) {
-    this.root = root;
-    this.sources = {};
-    this.traces = {};
-    this.importNr = 0;
-    this.core = new Core(plugins);
-    this.tokensByFile = {};
-  }
-
-  fetch(_newPath, relativeTo, _trace) {
-    let newPath = _newPath.replace(/^["']|["']$/g, ""),
-      trace = _trace || String.fromCharCode(this.importNr++);
-    return new Promise((resolve, reject) => {
-      let relativeDir = path.dirname(relativeTo),
-        rootRelativePath = path.resolve(relativeDir, newPath),
-        fileRelativePath = path.resolve(
-          path.join(this.root, relativeDir),
-          newPath
-        );
-
-      // if the path is not relative or absolute, try to resolve it in node_modules
-      if (newPath[0] !== "." && newPath[0] !== "/") {
-        try {
-          fileRelativePath = require.resolve(newPath);
-        } catch (e) {
-          // noop
-        }
+    // if the path is not relative or absolute, try to resolve it in node_modules
+    if (newPath[0] !== "." && newPath[0] !== "/") {
+      try {
+        absolutePath = require.resolve(newPath);
+      } catch (e) {
+        // noop
       }
+    }
 
-      const tokens = this.tokensByFile[fileRelativePath];
-      if (tokens) {
-        return resolve(tokens);
-      }
-
-      fs.readFile(fileRelativePath, "utf-8", (err, source) => {
-        if (err) reject(err);
-        this.core
-          .load(source, rootRelativePath, trace, this.fetch.bind(this))
-          .then(({ injectableSource, exportTokens }) => {
-            this.sources[fileRelativePath] = injectableSource;
-            this.traces[trace] = fileRelativePath;
-            this.tokensByFile[fileRelativePath] = exportTokens;
-            resolve(exportTokens);
-          }, reject);
-      });
+    fs.readFile(absolutePath, "utf-8", (err, source) => {
+      if (err) reject(err);
+      else resolve(source);
     });
-  }
-
-  get finalSource() {
-    const traces = this.traces;
-    const sources = this.sources;
-    let written = new Set();
-
-    return Object.keys(traces)
-      .sort(traceKeySorter)
-      .map((key) => {
-        const filename = traces[key];
-        if (written.has(filename)) {
-          return null;
-        }
-        written.add(filename);
-
-        return sources[filename];
-      })
-      .join("");
-  }
+  });
 }
--- src/css-loader-core/parser.js
+++ src/css-loader-core/parser.js
@@ -2,14 +2,22 @@
 
 const importRegexp = /^:import\((.+)\)$/;
 import replaceSymbols from "icss-replace-symbols";
+import postcss from 'postcss';
+import path from "path";
 
 export default class Parser {
-  constructor(pathFetcher, trace) {
+  constructor(root, plugins, pathFetcher, trace, ctx = {}) {
+    this.root = root;
+    this.plugins = plugins;
     this.pathFetcher = pathFetcher;
     this.plugin = this.plugin.bind(this);
     this.exportTokens = {};
     this.translations = {};
     this.trace = trace;
+
+    this.sources = ctx.sources || {};
+    this.traces = ctx.traces || {};
+    this.tokensByFile = ctx.tokensByFile || {};
   }
 
   plugin() {
@@ -65,16 +73,87 @@ export default class Parser {
   fetchImport(importNode, relativeTo, depNr) {
     let file = importNode.selector.match(importRegexp)[1],
       depTrace = this.trace + String.fromCharCode(depNr);
-    return this.pathFetcher(file, relativeTo, depTrace).then(
-      (exports) => {
-        importNode.each((decl) => {
-          if (decl.type == "decl") {
-            this.translations[decl.prop] = exports[decl.value];
-          }
-        });
-        importNode.remove();
-      },
-      (err) => console.log(err)
-    );
+    let newPath = file.replace(/^["']|["']$/g, "");
+    let relativeDir = path.dirname(relativeTo),
+      rootRelativePath = path.resolve(relativeDir, newPath),
+      fileRelativePath = path.resolve(
+        path.join(this.root, relativeDir),
+        newPath
+      );
+    let subParser = new Parser(this.root, this.plugins, this.pathFetcher, depTrace, {
+      sources: this.sources,
+      traces: this.traces,
+      tokensByFile: this.tokensByFile
+    })
+    const tokens = this.tokensByFile[fileRelativePath]
+
+    const base = tokens
+      ? Promise.resolve(tokens)
+      : this.pathFetcher(newPath, relativeTo, this.root)
+      .then(content => {
+        return postcss(this.plugins.concat([subParser.plugin()]))
+          .process(content, { from: "/" + rootRelativePath })
+          .then((result) => {
+            return {
+              injectableSource: result.css,
+              exportTokens: subParser.exportTokens,
+            };
+          })
+          .then(({ injectableSource, exportTokens }) => {
+            this.sources[fileRelativePath] = injectableSource;
+            this.traces[depTrace] = fileRelativePath;
+            this.tokensByFile[fileRelativePath] = exportTokens;
+            return exportTokens;
+          });
+      })
+    return base
+      .then(
+        (exports) => {
+          importNode.each((decl) => {
+            if (decl.type == "decl") {
+              this.translations[decl.prop] = exports[decl.value];
+            }
+          });
+          importNode.remove();
+        },
+        (err) => console.log(err)
+      );
+  }
+
+
+  get finalSource() {
+    const traces = this.traces;
+    const sources = this.sources;
+    let written = new Set();
+
+    return Object.keys(traces)
+      .sort(traceKeySorter)
+      .map((key) => {
+        const filename = traces[key];
+        if (written.has(filename)) {
+          return null;
+        }
+        written.add(filename);
+
+        return sources[filename];
+      })
+      .join("");
   }
 }
+
+// Sorts dependencies in the following way:
+// AAA comes before AA and A
+// AB comes after AA and before A
+// All Bs come after all As
+// This ensures that the files are always returned in the following order:
+// - In the order they were required, except
+// - After all their dependencies
+const traceKeySorter = (a, b) => {
+  if (a.length < b.length) {
+    return a < b.substring(0, a.length) ? -1 : 1;
+  } else if (a.length > b.length) {
+    return a.substring(0, b.length) <= b ? -1 : 1;
+  } else {
+    return a < b ? -1 : 1;
+  }
+};
