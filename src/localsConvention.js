18a6641d9da4ae7eea94305f00c0adbdb2009982
richardtmiles <richard@miles.systems>
2023-07-27 20:09:23 -0600

    git actions use https://nodejs.dev/en/about/releases/ timelines, localsConvention gets non breaking changes. Custom functions can return array of strings and new case statements locals

--- src/localsConvention.js
+++ src/localsConvention.js
@@ -10,12 +10,31 @@ export function makeLocalsConventionReducer(localsConvention, inputFile) {
 	return (tokens, [className, value]) => {
 		if (isFunc) {
 			const convention = localsConvention(className, value, inputFile);
+
+			if (Array.isArray(convention)) {
+
+				convention.forEach(convention => tokens[convention] = value);
+
+				return tokens;
+
+			}
+
 			tokens[convention] = value;
 
 			return tokens;
 		}
 
 		switch (localsConvention) {
+			case "none":
+				tokens[className] = value;
+				break;
+
+			case "all":
+				tokens[className] = value;
+				tokens[camelCase(className)] = value;
+				tokens[dashesCamelCase(className)] = value;
+				break;
+
 			case "camelCase":
 				tokens[className] = value;
 				tokens[camelCase(className)] = value;
